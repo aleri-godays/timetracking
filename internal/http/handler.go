7782e5b8966ea47135581ec124e04225c4a47869
Martin Radile <martin@radile.net>
2020-01-17 14:45:48 +0100

    task4

--- internal/http/handler.go
+++ internal/http/handler.go
@@ -3,6 +3,8 @@ package http
 import (
 	"github.com/aleri-godays/timetracking"
 	"github.com/labstack/echo/v4"
+	"net/http"
+	"strconv"
 )
 
 type timetrackingHandler struct {
@@ -10,21 +12,100 @@ type timetrackingHandler struct {
 }
 
 func (h *timetrackingHandler) Get(c echo.Context) error {
-	panic("implement me")
+	strID := c.Param("id")
+	id, err := strconv.Atoi(strID)
+	if err != nil {
+		return jsonError(c, "id must be an integer", http.StatusBadRequest)
+	}
+
+	entry, err := h.repo.Get(c.Request().Context(), id)
+	if err != nil {
+		return jsonError(c, "could not get entry", http.StatusInternalServerError)
+	}
+
+	if entry == nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	return c.JSONPretty(http.StatusOK, entry, "  ")
 }
 
 func (h *timetrackingHandler) Delete(c echo.Context) error {
-	panic("implement me")
+	strID := c.Param("id")
+	id, err := strconv.Atoi(strID)
+	if err != nil {
+		return jsonError(c, "id must be an integer", http.StatusBadRequest)
+	}
+
+	entry, err := h.repo.Get(c.Request().Context(), id)
+	if err != nil {
+		return jsonError(c, "could not delete entry", http.StatusInternalServerError)
+	}
+
+	if entry == nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	err = h.repo.Delete(c.Request().Context(), id)
+	if err != nil {
+		return jsonError(c, "could not delete entry", http.StatusInternalServerError)
+	}
+
+	return c.NoContent(http.StatusNoContent)
 }
 
 func (h *timetrackingHandler) Add(c echo.Context) error {
-	panic("implement me")
+	var entry timetracking.Entry
+	if err := c.Bind(&entry); err != nil {
+		return jsonError(c, "invalid request body", http.StatusBadRequest)
+	}
+
+	addedEntry, err := h.repo.Add(c.Request().Context(), &entry)
+	if err != nil {
+		return jsonError(c, "could not save entry", http.StatusInternalServerError)
+	}
+
+	type AddResponse struct {
+		ID int `json:"id"`
+	}
+	ar := AddResponse{ID: addedEntry.ID}
+
+	return c.JSON(http.StatusCreated, ar)
 }
 
 func (h *timetrackingHandler) Update(c echo.Context) error {
-	panic("implement me")
+	var entry timetracking.Entry
+	if err := c.Bind(&entry); err != nil {
+		return jsonError(c, "invalid request body", http.StatusBadRequest)
+	}
+	err := h.repo.Update(c.Request().Context(), &entry)
+	if err != nil {
+		return jsonError(c, "could not update entry", http.StatusInternalServerError)
+	}
+
+	return c.NoContent(http.StatusNoContent)
 }
 
 func (h *timetrackingHandler) All(c echo.Context) error {
-	panic("implement me")
+	entries, err := h.repo.All(c.Request().Context())
+	if err != nil {
+		return jsonError(c, "could not update entry", http.StatusInternalServerError)
+	}
+
+	user := c.QueryParam("user")
+	if user != "" {
+		filtered := make([]*timetracking.Entry, 0, 100)
+		for _, entry := range entries {
+			if entry.User == user {
+				filtered = append(filtered, entry)
+			}
+		}
+		entries = filtered
+	}
+
+	if len(entries) == 0 {
+		return c.JSON(http.StatusOK, []string{})
+	}
+
+	return c.JSON(http.StatusOK, entries)
 }
--- internal/http/routes.go
+++ internal/http/routes.go
@@ -3,4 +3,12 @@ package http
 //AddRoutes creates all routes
 func (s *Server) AddRoutes(handler timetrackingHandler) {
 	s.e.GET("/health", s.Health)
+
+	api := s.e.Group("/api/v1")
+
+	api.POST("/timetracking", handler.Add)
+	api.GET("/timetracking/:id", handler.Get)
+	api.PUT("/timetracking/:id", handler.Update)
+	api.DELETE("/timetracking/:id", handler.Delete)
+	api.GET("/timetracking", handler.All)
 }
